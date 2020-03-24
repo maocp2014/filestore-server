e3f685f85d6270a3691ff492ea70a6af77cd3459
Bryan <lancger@BryandeMacBook-Pro.local>
2020-03-24 09:08:04 +0800

    "云存储"系统原型之简单文件上传服务架构说明

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"filestore-server/handler"
+	"fmt"
+	"net/http"
+)
+
+func main(){
+	http.HandleFunc("/file/upload", handler.UploadHandler)
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil{
+		fmt.Printf("Failed to start server, err:%s", err.Error())
+	}
+}
